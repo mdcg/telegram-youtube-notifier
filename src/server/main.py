7e6be493bcd35a06c66477e8ca03d88c6ac72cf6
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-03-17 01:04:08 -0300

    feat: creating routes to handle with pubsubhubbub challenges and subscribe callback

--- src/server/main.py
+++ src/server/main.py
@@ -1,29 +1,52 @@
+import xml.etree.ElementTree as ET
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, status
 from pydantic import BaseModel
 from src.database.utils import search_for_subscribed_users
-from src.settings import TELEGRAM_TOKEN
-from telegram import Bot
+from src.server.bot import send_telegram_notification
 
 app = FastAPI()
 
 
-class Notification(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
 # https://pubsubhubbub.github.io/PubSubHubbub/pubsubhubbub-core-0.4.html#rfc.section.5
-# https://github.com/youtube/api-samples/issues/177
-# ToDo: Adicionar requests no src.bot.core para inscrever usuário
-@app.get("/notification")
-async def notify_users(notification: Notification):
-    bot = Bot(TELEGRAM_TOKEN)
-    return {"Hello": "World"}
+# https://pubsubhubbub.github.io/PubSubHubbub/pubsubhubbub-core-0.4.html#verifysub
+@app.get("/feed", status_code=200)
+async def feed_challenge(request: Request, response: Response):
+    challenge = request.query_params.get("hub.challenge")
+    if challenge:
+        return challenge
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.post("/feed", status_code=200)
+async def notify_users(request: Request, response: Response):
+    atom = "{http://www.w3.org/2005/Atom}"
+    yt = "{http://www.youtube.com/xml/schemas/2015}"
+
+    pubsubhubbub_request_body = await request.body()
+    root = ET.fromstring(pubsubhubbub_request_body)
+
+    entry = root.find(f"{atom}entry")
+    channel_id = entry.find(f"{yt}channelId").text
+    
+    author_tag = entry.find(f"{atom}author")
+    author = author_tag.find(f"{atom}name").text
+    link = entry.find(f"{atom}link").get("href")
+    title = entry.find(f"{atom}title").text
+
+    channels_id = search_for_subscribed_users(channel_id)
+    for ch_id in channels_id:
+        send_telegram_notification(ch_id, link, title, author)
+
+    return {
+        "channel_id": channel_id,
+        "link": link,
+        "title": title,
+        "author": author,
+    }
 
 
 if __name__ == "__main__":
