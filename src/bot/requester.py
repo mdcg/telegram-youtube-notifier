fb686ca4dda693e00b66b68e5831d65dae340f36
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-03-17 02:12:56 -0300

    feat: adding lease_seconds in subscribe request

--- src/bot/requester.py
+++ src/bot/requester.py
@@ -7,6 +7,7 @@ def subscribe_in_pubsubhubbub(channel_id):
         "hub.callback": CALLBACK_URL,
         "hub.topic": f"https://www.youtube.com/xml/feeds/videos.xml?channel_id={channel_id}",
         "hub.mode": "subscribe",
+        "hub.lease_seconds": 60 * 60 * 24 * 5,
     }
     r = requests.post('https://pubsubhubbub.appspot.com/subscribe', data=payload)
     return r.status_code
