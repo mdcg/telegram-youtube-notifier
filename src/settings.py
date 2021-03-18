8843ae4694926445e9fd79ccfa34a272ac58f921
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-03-18 00:51:08 -0300

    fix: cast PORT to int

--- src/settings.py
+++ src/settings.py
@@ -3,4 +3,4 @@ from decouple import config
 
 TELEGRAM_TOKEN = config("TELEGRAM_TOKEN", "")
 CALLBACK_URL = config("CALLBACK_URL", "")
-PORT = config("PORT", 5000)
+PORT = config("PORT", 5000, cast=int)
