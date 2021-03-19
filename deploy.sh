80b29dfb9e6b49cc69655914f1f5eccb31c4026a
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-03-19 00:16:36 -0300

    fix: changing heroku.yml bash command to sh, updating deploy.sh commands

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 heroku login
 heroku git:remote -a yt-shevchenko
 heroku stack:set container
-git push heroku master
-heroku ps:scale api=1
+git push heroku main
+heroku ps:scale web=1
 heroku ps:scale bot=1
+heroku logs --tail
